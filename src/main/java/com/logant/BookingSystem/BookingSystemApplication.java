1efa8087e5e378dd8f87895cba3c89cedbe07d91
Purwatsate <ant.pwt@gamil.com>
2024-11-15 04:29:33 +0630

    update

--- src/main/java/com/logant/BookingSystem/BookingSystemApplication.java
+++ src/main/java/com/logant/BookingSystem/BookingSystemApplication.java
@@ -4,13 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.logant.BookingSystem.config.RSAKeyRecord;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 
 
@@ -23,15 +17,4 @@ public class BookingSystemApplication {
 		SpringApplication.run(BookingSystemApplication.class, args);
 	}
 
-}
-
-@RestController
-@RequestMapping("test/user")
-class TestingController{
-
-	@GetMapping
-	public String postMethodName() {
-		return "Hello";
-	}
-	
 }
